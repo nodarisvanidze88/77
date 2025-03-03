64bdd045544eecc1076a13d928bf45bd1f841840
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2025-03-04 02:07:02 +0400

    try to fix image downloading

--- fileImageData/views.py
+++ fileImageData/views.py
@@ -288,12 +288,12 @@ def download_images_by_category_view_new(request):
         # file_names = list(file_names)
         # local_folder_path = f"./{category.category_name}"  # Local path for downloading files
         gsutil_download_multiple(bucket_name, file_names, local_folder_path)
-        # downloaded_files = os.listdir(local_folder_path)
+        downloaded_files = os.listdir(local_folder_path)
         # print(f"Files in {local_folder_path}: {downloaded_files}")
-        # if not downloaded_files:
-        #   return HttpResponse(f"Files were not downloaded. Check gsutil logs.", status=500)
-        # local_rar_file_path = f"{local_folder_path}.rar"
-        # create_rar_file(local_folder_path, local_rar_file_path)
+        if not downloaded_files:
+          return HttpResponse(f"Files were not downloaded. Check gsutil logs.", status=500)
+        local_rar_file_path = f"{local_folder_path}.rar"
+        create_rar_file(local_folder_path, local_rar_file_path)
         # if not os.path.exists(local_rar_file_path):
         #     return HttpResponse("RAR file was not created successfully.", status=500)
         # upload_file_to_gcs(bucket_name, local_rar_file_path, local_folder_path,rar_file_name)
